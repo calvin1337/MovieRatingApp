7bb1f3e2d36adb6f6ddf80a0fccc4c6e0266fef0
Calvin Donaldson <calvindonaldson1997313@gmail.com>
2020-07-01 12:55:45 +0100

    updates to movie modal

--- src/Components/MovieCard/MovieModal/MovieModal.js
+++ src/Components/MovieCard/MovieModal/MovieModal.js
@@ -10,20 +10,28 @@ export class MovieModal extends Component {
     
     render() {
         
-        let movieData = ""
+        let movieImage = ""
+        let movieTitle = ""
+        let movieRelease = ""
 
         if(this.props.movie !== ""){
-            movieData = 
-            (<div className="modal-box">
-            <h1>{this.props.movie.title}</h1>
-            </div>)
+            movieImage = this.props.movie.image
+            movieTitle = this.props.movie.title
+            movieRelease = this.props.movie.release
         }
 
        
 
         return (
             <div  onClick={(e) => this.props.toggleModal(e)} style={this.props.show === true ? {display:"block"} : null} className="movie-modal">
-                    {movieData}
+                    <div className="modal-box">
+                    <div class="MovieModalImage"><img src={movieImage} alt="Movie Poster" /></div>
+                    <div class="MovieModalInfo">
+                    <h1>{movieTitle}</h1>
+                    <h6>{movieRelease} | Action / Thriller | 1h 23m</h6>
+                    </div>
+                    </div>
+                    
                 
             </div>
         )
